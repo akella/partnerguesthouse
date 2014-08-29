ff8aa39f28250b0c03511b3cf61d8e9ff7357ca0
Valera19900 <kaktuz34@gmail.com>
2014-08-29 18:02:59 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -115,39 +115,39 @@ head.ready(function() {
 	});
 
 
-	// 	$(function(){
-	// 		window.prettyPrint && prettyPrint();
-	// 		$('#dp1').datepicker();
-	// 		$('#dp2').datepicker();
-	// 		$('#dp3').datepicker();
-	// 		$('#dp4').datepicker();
+		// $(function(){
+		// 	window.prettyPrint && prettyPrint();
+		// 	$('#dp1').datepicker();
+		// 	$('#dp2').datepicker();
+		// 	$('#dp3').datepicker();
+		// 	$('#dp4').datepicker();
 			
 
- //        // disabling dates
- //        var nowTemp = new Date();
- //        var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+  //       // disabling dates
+  //       var nowTemp = new Date(); 
+  //       var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
 
- //        var checkin = $('#dpd1').datepicker({
- //          onRender: function(date) {
- //            return date.valueOf() < now.valueOf() ? 'disabled' : '';
- //          }
- //        }).on('changeDate', function(ev) {
- //          if (ev.date.valueOf() > checkout.date.valueOf()) {
- //            var newDate = new Date(ev.date)
- //            newDate.setDate(newDate.getDate() + 1);
- //            checkout.setValue(newDate);
- //          }
- //          checkin.hide();
- //          $('#dpd3')[0].focus();
- //        }).data('datepicker');
- //        var checkout = $('#dpd3').datepicker({
- //          onRender: function(date) {
- //            return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
- //          }
- //        }).on('changeDate', function(ev) {
- //          checkout.hide();
- //        }).data('datepicker');
-	// 	});
+  //       var checkin = $('#dpd1').datepicker({
+  //         onRender: function(date) {
+  //           return date.valueOf() < now.valueOf() ? 'disabled' : '';
+  //         }
+  //       }).on('changeDate', function(ev) {
+  //         if (ev.date.valueOf() > checkout.date.valueOf()) {
+  //           var newDate = new Date(ev.date)
+  //           newDate.setDate(newDate.getDate() + 1);
+  //           checkout.setValue(newDate);
+  //         }
+  //         checkin.hide();
+  //         $('#dpd3')[0].focus();
+  //       }).data('datepicker');
+  //       var checkout = $('#dpd3').datepicker({
+  //         onRender: function(date) {
+  //           return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+  //         }
+  //       }).on('changeDate', function(ev) {
+  //         checkout.hide();
+  //       }).data('datepicker');
+		// });
 
 	// $(".js-datepicker").focus(function(){
 	// 	$('.datepicker').slideTogle();
